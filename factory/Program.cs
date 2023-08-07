8d7ed183917107040d7e6e43e6b4db354a93916f
MRY\annie.liang <annie.liang@mry.com.tw>
2023-08-07 11:56:37 +0800

    simple factory

---
+++ factory/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.InteropServices;
+
+Factory.CreateProduct("A");
+
+
+class Factory
+{
+
+
+    public static Product CreateProduct(string type)
+    {
+        Product p = null;
+        switch (type)
+        {
+            case "A":
+                p = new ProductA();
+                break;
+            case "B":
+                p = new ProductB();
+                break;
+            default:
+                throw new Exception("Error");
+        }
+        return p.CreateObj();
+    }
+
+
+}
+
+abstract class Product
+{
+    abstract public Product CreateObj();
+}
+
+class ProductA : Product
+{
+    public override Product CreateObj()
+    {
+        Console.WriteLine("ProductA");
+        return new ProductA();
+    }
+}
+class ProductB : Product
+{
+    public override Product CreateObj()
+    {
+        Console.WriteLine("ProductB");
+        return new ProductB();
+    }
+}
